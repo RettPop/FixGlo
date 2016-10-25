0e7426466343833cfa3b1c8603fa6fa14da3ce38
rettpop <rettpop@leftbit.com>
2016-10-25 14:26:43 +0300

    New icon. Icon displayed in address bar. Icon displaying being initialized by message from content script

--- fixglo.js
+++ fixglo.js
@@ -1,37 +1,28 @@
 'use strict';
 
-/*
-Each time a tab is updated, reset the page action for that tab.
-*/
-fixGlo();
-
-browser.runtime.sendMessage({
-  type: "notify-attached-tab",
-});
-
-
-
-function fixGlo()
-{
-    [].slice.call(document.styleSheets)
-            .map(   function(styleSheet)
-                    {
-                        // processing only Globallogic' sheets
-                        if (   styleSheet.href.match(/globallogic/gi)
-                            && styleSheet.cssRules )
+(function()
+    {
+        browser.runtime.sendMessage( {"showIcon" : true} ); // show plugin icon
+        [].slice.call(document.styleSheets)
+                .map(   function(styleSheet)
                         {
-                            [].slice.call(styleSheet.cssRules)
-                                    .map(   function (cssRule)
-                                            {
-                                                if(     cssRule.style !== undefined
-                                                    && cssRule.style["word-break"] !== undefined )
+                            // processing only Globallogic' sheets
+                            if (   styleSheet.href.match(/globallogic/gi)
+                                && styleSheet.cssRules )
+                            {
+                                [].slice.call(styleSheet.cssRules)
+                                        .map(   function (cssRule)
                                                 {
-                                                    //console.log(cssRule.style["word-break"]); //speedwaster
-                                                    cssRule.style["word-break"] = "unset";
+                                                    if(     cssRule.style !== undefined
+                                                        && cssRule.style["word-break"] !== undefined )
+                                                    {
+                                                        //console.log(cssRule.style["word-break"]); //speed eater
+                                                        cssRule.style["word-break"] = "unset";
+                                                    }
                                                 }
-                                            }
-                                    );
+                                        );
+                            }
                         }
-                    }
-            );
-}
+                );
+    }
+)();
--- showicon.js
+++ showicon.js
@@ -1,13 +1,9 @@
-
 "use strict";
 
-browser.runtime.onMessage.addListener(msg => {
-  chrome.pageAction.show(chrome.tabs.getCurrent().id);
-});
-
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (tab.url.match(/glo\.globallogic\.com/gi)) {
-    chrome.pageAction.show(tab.id);
-  }
-});
+browser.runtime.onMessage.addListener(function(msg, sender)
+                                    {
+                                        // display plugin icon
+                                        if( msg.showIcon == true ) {
+                                            chrome.pageAction.show(sender.tab.id);
+                                        }
+                                    });
